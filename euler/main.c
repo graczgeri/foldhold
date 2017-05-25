8298610945992f12115ab6afffe763a645aaa1d0
graczgeri <graczgeri@gmail.com>
2017-05-25 13:12:14 +0200

    1.1

--- euler/main.c
+++ euler/main.c
@@ -5,3 +5,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+int main()
+{
+    double Mf=5.9736e+24;
+    double Mh=7.349e+22;
+    double apog=405500000;
+    double v_apog=964;
+    double peri=363300000;
+    double v_peri=1076;
+    double G=6.67384e-11;
+
+    int step=3600*24*30;
+    double h=1200.0;
+
+    double* x=(double*)calloc(step, sizeof(double));
+    double* y=(double*)calloc(step, sizeof(double));
+
+    x[0]=0;
+    y[0]=apog;
+
+    euler(x,y,h,v_apog,0,step);
+    printf("az euler.dat tartalmazza a koordinatakat");
+    return 0;
+
+}
--- rk4/main.c
+++ rk4/main.c
@@ -6,3 +6,36 @@
 #include <stdlib.h>
 #include <math.h>
 
+
+
+
+
+
+int main()
+{
+    double Mf=5.9736*pow(10,24);
+    double Mh=7.349*pow(10,22);
+    
+    size_t r_size = 9;
+    
+    double* r=(double*)malloc(r_size*sizeof(double));
+    r[0]=0.0; //t ido
+    r[1]=0.0; //Hold v_x
+    r[2]=968; // Hold v_y Foldtavolban
+    r[3]=0.0; //Fold v_x
+    r[4]=-Mh/Mf*964 ; //Fold v_y
+    r[5]=-405500000; //Hold x Foldtavolban
+    r[6]=0.0; // Hold y
+    r[7]=0.0; // Fold x
+    r[8]=0.0; // Fold y
+    double h=100;
+    int n=60*24*30;
+
+    printf("%d", n);
+    
+    double* matrix=(double*)malloc(n*9*sizeof(double));
+    double* k1=(double*)malloc(n*sizeof(double));
+    double* k2;
+    double* k3;
+    double* k4;
+    double b;
