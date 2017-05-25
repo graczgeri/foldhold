b58bd45e18ed1073bcd2b551c7fb7a08c03c9450
graczgeri <graczgeri@gmail.com>
2017-05-25 15:43:18 +0200

    a

--- rk4/main.c
+++ rk4/main.c
@@ -65,12 +65,12 @@ int main()
     r[0]=0.0; //t ido
     r[1]=0.0; //Hold v_x
     r[2]=968; // Hold v_y Foldtavolban
-    r[3]=0.0; //F�ld v_x
-    r[4]=-Mh/Mf*964 ; //F�ld v_y
+    r[3]=0.0; //Fold v_x
+    r[4]=-Mh/Mf*964 ; //Fld v_y
     r[5]=-405500000; //Hold x Foldtavolban
     r[6]=0.0; // Hold y
-    r[7]=0.0; // F�ld x
-    r[8]=0.0; // F�ld y
+    r[7]=0.0; // Fold x
+    r[8]=0.0; // Fold y
     double h=100;
     int n=60*24*30;
 
